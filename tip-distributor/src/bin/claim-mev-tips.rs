9fc529d478bdbe0286564348d90121b1586e08aa
Jed <4679729+jedleggett@users.noreply.github.com>
2025-03-21 10:43:56 -0500

    hack

--- tip-distributor/src/bin/claim-mev-tips.rs
+++ tip-distributor/src/bin/claim-mev-tips.rs
@@ -10,10 +10,7 @@ use {
         signature::{read_keypair_file, Keypair},
     },
     solana_tip_distributor::{
-        claim_mev_workflow::{claim_mev_tips, ClaimMevError},
-        read_json_from_file,
-        reclaim_rent_workflow::reclaim_rent,
-        GeneratedMerkleTreeCollection,
+        claim_mev_workflow::ClaimMevError, reclaim_rent_workflow::reclaim_rent,
     },
     std::{
         path::PathBuf,
@@ -25,10 +22,6 @@ use {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Path to JSON file containing the [GeneratedMerkleTreeCollection] object.
-    #[arg(long, env)]
-    merkle_trees_path: PathBuf,
-
     /// RPC to send transactions through
     #[arg(long, env, default_value = "http://localhost:8899")]
     rpc_url: String,
@@ -45,9 +38,9 @@ struct Args {
     #[arg(long, env, default_value_t = 60 * 60)]
     max_retry_duration_secs: u64,
 
-    /// Specifies whether to reclaim any rent.
-    #[arg(long, env, default_value_t = true)]
-    should_reclaim_rent: bool,
+    /// Specifies which epoch to reclaim any.
+    #[arg(long, env)]
+    reclaim_rent_epoch: u64,
 
     /// Specifies whether to reclaim rent on behalf of validators from respective TDAs.
     #[arg(long, env)]
@@ -56,47 +49,6 @@ struct Args {
     /// The price to pay for priority fee
     #[arg(long, env, default_value_t = 1)]
     micro_lamports: u64,
-}
-
-async fn start_mev_claim_process(
-    merkle_trees: GeneratedMerkleTreeCollection,
-    rpc_url: String,
-    tip_distribution_program_id: Pubkey,
-    signer: Arc<Keypair>,
-    max_loop_duration: Duration,
-    micro_lamports: u64,
-) -> Result<(), ClaimMevError> {
-    let start = Instant::now();
-
-    match claim_mev_tips(
-        &merkle_trees,
-        rpc_url,
-        tip_distribution_program_id,
-        signer,
-        max_loop_duration,
-        micro_lamports,
-    )
-    .await
-    {
-        Err(e) => {
-            datapoint_error!(
-                "claim_mev_workflow-claim_error",
-                ("epoch", merkle_trees.epoch, i64),
-                ("error", 1, i64),
-                ("err_str", e.to_string(), String),
-                ("elapsed_us", start.elapsed().as_micros(), i64),
-            );
-            Err(e)
-        }
-        Ok(()) => {
-            datapoint_info!(
-                "claim_mev_workflow-claim_completion",
-                ("epoch", merkle_trees.epoch, i64),
-                ("elapsed_us", start.elapsed().as_micros(), i64),
-            );
-            Ok(())
-        }
-    }
 }
 
 async fn start_rent_claim(
@@ -151,36 +103,21 @@ async fn main() -> Result<(), ClaimMevError> {
 
     let args: Args = Args::parse();
     let keypair = Arc::new(read_keypair_file(&args.keypair_path).expect("read keypair file"));
-    let merkle_trees: GeneratedMerkleTreeCollection =
-        read_json_from_file(&args.merkle_trees_path).expect("read GeneratedMerkleTreeCollection");
     let max_loop_duration = Duration::from_secs(args.max_retry_duration_secs);
 
-    info!(
-        "Starting to claim mev tips for epoch: {}",
-        merkle_trees.epoch
-    );
-    let epoch = merkle_trees.epoch;
+    let epoch = args.reclaim_rent_epoch;
+    info!("Starting to reclaiming for epoch: {}", epoch);
 
     let mut futs = vec![];
-    futs.push(tokio::spawn(start_mev_claim_process(
-        merkle_trees,
+    futs.push(tokio::spawn(start_rent_claim(
         args.rpc_url.clone(),
         args.tip_distribution_program_id,
         keypair.clone(),
         max_loop_duration,
+        args.should_reclaim_tdas,
         args.micro_lamports,
+        epoch,
     )));
-    if args.should_reclaim_rent {
-        futs.push(tokio::spawn(start_rent_claim(
-            args.rpc_url.clone(),
-            args.tip_distribution_program_id,
-            keypair.clone(),
-            max_loop_duration,
-            args.should_reclaim_tdas,
-            args.micro_lamports,
-            epoch,
-        )));
-    }
     let results = join_all(futs).await;
     solana_metrics::flush(); // sometimes last datapoint doesn't get emitted. this increases likelihood.
     for r in results {
