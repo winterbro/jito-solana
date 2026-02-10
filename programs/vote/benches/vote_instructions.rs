bc38d5e8740c185099efae23dc26c2ae4cd5a330
Zed Jiang <zed@orca.so>
2026-02-09 23:54:56 -0800

    opt

--- programs/vote/benches/vote_instructions.rs
+++ programs/vote/benches/vote_instructions.rs
@@ -70,7 +70,7 @@ fn create_accounts() -> (
         ));
 
         for next_vote_slot in 0..num_initial_votes {
-            vote_state.process_next_vote_slot(next_vote_slot, 0, 0);
+            vote_state.process_next_vote_slot(next_vote_slot, 0, 0, true);
         }
         let mut vote_account_data: Vec<u8> = vec![0; VoteStateV4::size_of()];
         let versioned = VoteStateVersions::new_v4(vote_state.unwrap_v4());
